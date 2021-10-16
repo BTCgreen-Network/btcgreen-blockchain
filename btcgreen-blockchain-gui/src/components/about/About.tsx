b43fe293dc4019d122ff761ff38082a0415d7a88
BTCGreen-Network <svginsomnia@gmail.com>
2021-10-16 01:35:58 -0400

    Rename

--- btcgreen-blockchain-gui/src/components/about/About.tsx
+++ btcgreen-blockchain-gui/src/components/about/About.tsx
@@ -114,7 +114,7 @@ export default function About(props: Props) {
         </StyledLink>
         <StyledSubTitle>{description}</StyledSubTitle>
         <Spacer />
-        <div className="copyright">Copyright (c) 2021 Pink Sheets Crypto</div>
+        <div className="copyright">Copyright (c) 2021 BTCgreen Network</div>
         <Spacer />
         <VersionsTable>
           {versions?.electron && (
