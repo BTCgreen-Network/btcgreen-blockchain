05ba54dc0b8e8424bc558a6c242765b1aaf717f8
BTCgreen-Network <svginsomnia@gmail.com>
2022-03-18 08:07:52 -0400

    Chia 1.3.1 Update

--- btcgreen-blockchain-gui/packages/gui/src/components/block/Block.jsx
+++ btcgreen-blockchain-gui/packages/gui/src/components/block/Block.jsx
@@ -26,6 +26,7 @@ import {
   mojoToBTCgreen,
   DashboardTitle,
   Suspender,
+  toBech32m,
 } from '@btcgreen/core';
 import {
   unix_to_short_date,
@@ -33,7 +34,6 @@ import {
   arr_to_hex,
   sha256,
 } from '../../util/utils';
-import toBech32m from '../../util/toBech32m';
 import BlockTitle from './BlockTitle';
 
 /* global BigInt */
--- btcgreen-blockchain-gui/packages/icons/src/BTCgreen.tsx
+++ btcgreen-blockchain-gui/packages/icons/src/BTCgreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SvgIcon, SvgIconProps } from '@material-ui/core';
-import SHIBgreenIcon from './images/btcgreen.svg';
+import BTCgreenIcon from './images/btcgreen.svg';
 
 export default function Keys(props: SvgIconProps) {
-  return <SvgIcon component={SHIBgreenIcon} viewBox="0 0 150 58" {...props} />;
+  return <SvgIcon component={BTCgreenIcon} viewBox="0 0 150 58" {...props} />;
 }
